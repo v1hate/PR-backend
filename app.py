7c1f023812ef9b2841abad3cc0fdf71d57381d29
v1hate <herrero.julian.galeano@gmail.com>
2024-10-21 09:45:28 -0300

    añadiendo flask cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Importa CORS
 import file_handler
 import socket
 
 app = Flask(__name__)
+CORS(app)  # Activa CORS en todas las rutas
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
